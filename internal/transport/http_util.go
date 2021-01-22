6c1ab1a8b44fccd819122037dcb85ef762b26c0b
sandertungeaspoy <35257578+sandertungeaspoy@users.noreply.github.com>
2021-01-22 09:57:08 +0100

    Update http_util.go

--- internal/transport/http_util.go
+++ internal/transport/http_util.go
@@ -34,12 +34,12 @@ import (
 	"unicode/utf8"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/sandertungeaspoy/grpc-go/internal/grpcutil"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/hpack"
 	spb "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc/internal/grpcutil"
 	"google.golang.org/grpc/status"
 )
 
